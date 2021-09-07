fd4c9c9df3dcd57288340f7e520743ff94e8b958
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-09-07 00:59:44 -0700

    mess of new scripts to display colors

--- make-font-html.sh
+++ make-font-html.sh
@@ -1,20 +1,20 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-cat << __HEADER
+cat > fonts.html << __HEADER
 <!DOCTYPE html>
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Sample of local fonts matching '$1'</title>
 </head>
-<body>
+<body style="  display: grid;  grid-template-columns: repeat(5, 366.7px); border: 2px solid #000 ">
 __HEADER
 
 fc-list --format='%{family}\n' $1 | sort -u | while IFS='' read -r fontfamily
 do
-    cat << __BODY
-    <hr/>
-    <div style="font-family: '${fontfamily}', 'serif'">
+    cat >> fonts.html << __BODY
+    <div style="font-family: '${fontfamily}', 'serif'; border: 3px solid #000; border-collapse:collapse; overflow-wrap: break-word; ">
+
         <h1>${fontfamily}</h1>
         <p>
             The quick brown fox jumped over the lazy brown dog<br/>
@@ -25,8 +25,12 @@ __BODY
 
 done
 
-cat << __FOOTER
+cat >> fonts.html << __FOOTER
     <hr/>
 </body>
 </html>
 __FOOTER
+
+echo "fonts.html created"
+
+firefox fonts.html
